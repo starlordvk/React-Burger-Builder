80f78aa8cfb47c284f52fd623348fca6e28128e4
starlordvk <chocoshake2007@gmail.com>
2018-10-20 13:55:29 +0530

    adding dynamic ingredients to burger components from burger builder state

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+
+const burger = (props) => {
+
+    //get ingredinets based on the ingredient key and values in the js object in burger builder
+    const transformedIngredients = Object.keys(props.ingredients).map( igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i ) => {
+            return <BurgerIngredient key={igKey + i} type= {igKey}></BurgerIngredient>
+        });
+    });
+
+    console.log(transformedIngredients);
+
+    return (
+        <div className= {classes.Burger}>
+            <BurgerIngredient type='bread-top'></BurgerIngredient>
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom'></BurgerIngredient>
+        </div>
+    );
+}
+export default burger;
