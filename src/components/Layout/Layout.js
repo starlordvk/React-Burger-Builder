b6c56df1db46ae15a9cf838dc58c6c00f80379b1
starlordvk <chocoshake2007@gmail.com>
2018-10-20 11:42:53 +0530

    adding layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const layout = ( props ) => (
+    <Aux>
+        <div>ToolBar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
