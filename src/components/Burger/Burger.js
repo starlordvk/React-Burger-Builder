0328a6eda93410b5ffe06ebc362c81c8ea7bcdda
starlordvk <chocoshake2007@gmail.com>
2018-10-20 19:12:08 +0530

    add build control components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,13 +6,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
 
     //get ingredinets based on the ingredient key and values in the js object in burger builder
-    const transformedIngredients = Object.keys(props.ingredients).map( igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map( igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i ) => {
             return <BurgerIngredient key={igKey + i} type= {igKey}></BurgerIngredient>
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
 
     console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients</p>
+    }
 
     return (
         <div className= {classes.Burger}>
